d36d4b3004cae93ace929627ce8c34b0fc289b34
iamakasha <iamakasha2020@gmail.com>
2025-11-16 11:48:14 +0530

    Add files via upload

---
+++ ARRAY ELEMENTS .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int *ptr;
+    int i;
+
+    ptr = arr; // pointer points to the first element of the array
+
+    printf("Accessing array elements using pointer:\n");
+
+    for(i = 0; i < 5; i++) {
+        printf("Element %d = %d (Address = %p)\n", i, *(ptr + i), (ptr + i));
+    }
+
+    return 0;
+}
